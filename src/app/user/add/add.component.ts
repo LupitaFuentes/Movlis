7eba5e7e7484fb0d7dbac1e38549046157321bc3
LupitaFuentes <83789201+LupitaFuentes@users.noreply.github.com>
2022-12-14 15:35:34 -0600

    add

--- src/app/user/add/add.component.ts
+++ src/app/user/add/add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User, UserService} from '../../services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  user:User={
+    id_user:0,
+    name:'',
+    pass:''
+  };
+
+  constructor(private UserService:UserService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  add(){
+    delete this.user.id_user;
+    this.UserService.addUser(this.user).subscribe();
+    this.router.navigate(['/home']);
   }
 
 }
